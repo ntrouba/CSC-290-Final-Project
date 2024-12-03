4f79aa815f8f57d75cded46a0b6361b6468d430a
Hillary Dunkley <hvgdunkley@gmail.com>
2024-12-03 13:00:05 -0500

    changed state class

--- main.py
+++ main.py
@@ -5,16 +5,31 @@ class State:
     self.vowel_path = ""
     self.consonant = ""
     self.accept = accept
-  def set_vowel(self, state)-> str:
-    self.vowel_path= state
-    return self.vowel_path
-  def set_consonant(self, state) -> str: 
-    self.consonant = state
-    return self.consonant
-  def get_vowel(self): 
-    return self.vowel_path
-  def get_consonant(self):
-    return self.consonant
+  def set_noun(self, state)-> str:
+    self.noun_path= state
+    return self.noun_path
+  def set_verb(self, state) -> str: 
+    self.verb = state
+    return self.verb
+  def set_adjective(self, state) -> str:
+    self.adjective = state 
+    return self.adjective 
+  def set_article(self, state) -> str: 
+    self.article = state
+    return self.article
+  def set_preposition(self, state) -> str: 
+    self.preposition = state
+    return self.preposition
+  def get_noun(self): 
+    return self.noun_path
+  def get_verb(self):
+    return self.verb
+  def get_adjective(self): 
+    return self.adjective
+  def get_article(self): 
+    return self.article
+  def get_preposition(self): 
+    return self.preposition
   def is_accept(self): 
     return self.accept 
     
@@ -24,6 +39,13 @@ class Fsa:
        self.states = [] 
        self.consonants = ["z", "t", "n", "h", "d", "sh", "th", "ch", "b", "w", "x"]
        self.vowels = ["a", "e", "i", "o", "u", "y", ""]
+       #nouns
+       #verbs
+       #adjectives
+       #articles 
+       #prepositions
+       #auxiliary verbs 
+
 
     def get_start(self):
         return self.start_state 
@@ -89,11 +111,11 @@ def main():
     s4 = State(True)
     
     # set paths 
-    s1.set_consonant(s2)
-    s1.set_vowel(s2)
-    s2.set_vowel(s3)
-    s3.set_consonant(s4)
-    s4.set_vowel(s3)
+    # s1.set_consonant(s2)
+    # s1.set_vowel(s2)
+    # s2.set_vowel(s3)
+    # s3.set_consonant(s4)
+    # s4.set_vowel(s3)
 
     # store states in a list 
     states = [s1, s2, s3, s4]
