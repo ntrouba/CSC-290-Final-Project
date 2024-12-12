abab48b2138db0791fb6aeb305951468c9cd60c6
Priya Dalal-Whelan <priyadw00@gmail.com>
2024-12-12 18:28:40 -0500

    Update main.py
    recursion functional

--- main.py
+++ main.py
@@ -8,18 +8,25 @@ class State:
         self.transitions:dict[str, list[State]] | None = None 
 
     def choose(self) -> tuple[str, Self|None]:
+        
+        # if self.accept and there are no transitions, have to accept. otherwise, choose randomly
+        #whether or not to be done generating 
         if self.accept: 
-            stop = random.randint(0,1)
+            if len(list(self.transitions.keys()))== 0:
+                stop = True 
+            else:
+                stop = random.randint(0,1)
             if stop: 
                 return "stop", None 
+          
         else:
             transition:str = random.choice(list(self.transitions.keys()))
             print(transition)
             next_state:State = random.choice(self.transitions[transition])
             return transition, next_state
 
-    def __str__()
-        print accept
+    def __str__(self):
+        return str(self.accept) + str(self.transitions)
     # def choose 
         # returns transition str, and next state 
 
@@ -28,8 +35,17 @@ class State:
     
 class Fsa:
     def __init__(self, start_state:State, states:list[State]): 
-       self.start_state = start_state 
-       self.states = [] 
+       self.start_state:State = start_state 
+       self.states:list[State] = [] 
+
+       self.word_bank:dict[str, list[str] |State] = { "noun": ["reindeer", "misteltoe", "stockings", "chimney", "elf"], 
+       "adj": ["warm", "cold", "happy", "festive", "christmassy"], 
+       "articles" : ["a", "the"], 
+       "prep": ["above", "with", "on", "between", "at"], 
+       "det" : ["can", "might", "will"],
+       # epsiolon jump 
+       "e" : [""]
+       }
 
        # transitions dictionary 
 
@@ -43,20 +59,19 @@ class Fsa:
 
 
 
+    #    self.nouns = ["reindeer", "misteltoe", "stockings", "chimney", "elf"]
+    #    self.verbs = ["caroling", "sipping", "gifting", "opening", "kissing"]
+    #    self.adjectives = ["warm", "cold", "happy", "festive", "christmassy"]
+    #    self.articles = ["a", "the"]
+    #    self.prepositions = ["above", "with", "on", "between", "at"]
+    #    self.determiners = ["can", "might", "will"]
 
-       self.nouns = ["reindeer", "misteltoe", "stockings", "chimney", "elf"]
-       self.verbs = ["caroling", "sipping", "gifting", "opening", "kissing"]
-       self.adjectives = ["warm", "cold", "happy", "festive", "christmassy"]
-       self.articles = ["a", "the"]
-       self.prepositions = ["above", "with", "on", "between", "at"]
-       self.determiners = ["can", "might", "will"]
-
-
-    
 
     
 
-def gen(fsa:Fsa)-> str:
+    
+
+def gen(fsa:Fsa, fsa_dict:dict[str, Fsa])-> str:
     
     sentence:str= ""
     stop = False
@@ -72,21 +87,35 @@ def gen(fsa:Fsa)-> str:
         else:
 
             ret = current_state.choose()
+            #print(ret)
+            #print("type of ret: ", type(ret))
             transition:str = ret[0]
             if transition == "stop":
                 stop = True
+                #print("break")
+                break
 
             next_state:State|None = ret[1]
 
-            print("transition: ", transition, "next state:" , next_state)
+           # print("transition: ", transition, "next state:" , next_state)
 
-            sentence += transition 
+            # if transition is terminal, add word from list 
+
+            # if transition is an fsa (nonterminal)
+                #sentence += gen(transition)
+            if fsa_dict.get(transition) != None: 
+                print("reciursive call to:", transition)
+                sentence += gen(fsa_dict[transition], fsa_dict)
+            elif fsa.word_bank.get(transition) != None:
+                word:str = random.choice(list(fsa.word_bank.get(transition)))
+                print("word:", word)
+                sentence += word
+            else: 
+                print("error, bad transition")
+            
             sentence += ' '
-            current_state = next_state 
 
-
-
-
+            current_state:State = next_state 
 
 
 
@@ -111,12 +140,6 @@ def gen(fsa:Fsa)-> str:
 
 
 
-
-
-
-
-
-        
 
 
 
@@ -162,25 +185,28 @@ def main():
     s3.transitions = {"noun" : [s4]} 
     s4.transitions = {}
 
-
-
-
-
-    # set paths 
-    # s1.set_consonant(s2)
-    # s1.set_vowel(s2)
-    # s2.set_vowel(s3)
-    # s3.set_consonant(s4)
-    # s4.set_vowel(s3)
-
     # store states in a list 
-    states = [s1, s2, s3, s4]
-
+    np_states = [s1, s2, s3, s4]
     #create fsa 
-    fsa = Fsa(s1, states)
+    np:Fsa = Fsa(s1, np_states)
+
+    s5 = State(False)
+    s6 = State(True)
+
+    s_states = [s5, s6]
+
+    s5.transitions = {"np":[s6]} 
+    s6.transitions = {}
+
+    s:Fsa = Fsa(s5, s_states)
+
+    fsa_dict:dict[str, Fsa] = {"np": np, "s": s}
+
+
+
 
     #generate 10 workds 
-    sentence = gen(fsa)
+    sentence = gen(s, fsa_dict)
     print(sentence)
 
 
